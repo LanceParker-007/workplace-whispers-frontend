61b78fb390d5f5be6ba6373f21f5f881268144ff
lance <lanceparker2001@gmail.com>
2024-07-28 22:04:08 +0530

    Images fixed 1

--- src/assets/assets.jsx
+++ src/assets/assets.jsx
@@ -1,6 +1,10 @@
-import image1 from "https://unity-games-test-bucket.s3.ap-south-1.amazonaws.com/workplace-whispers/images/image1.png";
-import image2 from "https://unity-games-test-bucket.s3.ap-south-1.amazonaws.com/workplace-whispers/images/image2.png";
-import windIconPng from "https://unity-games-test-bucket.s3.ap-south-1.amazonaws.com/workplace-whispers/images/windIcon.png";
-import windIconSVG from "https://unity-games-test-bucket.s3.ap-south-1.amazonaws.com/workplace-whispers/images/windIcon.svg";
+const image1 =
+  "https://unity-games-test-bucket.s3.ap-south-1.amazonaws.com/workplace-whispers/images/image1.png";
+const image2 =
+  "https://unity-games-test-bucket.s3.ap-south-1.amazonaws.com/workplace-whispers/images/image2.png";
+const windIconPng =
+  "https://unity-games-test-bucket.s3.ap-south-1.amazonaws.com/workplace-whispers/images/windIcon.png";
+const windIconSVG =
+  "https://unity-games-test-bucket.s3.ap-south-1.amazonaws.com/workplace-whispers/images/windIcon.svg";
 
 export const icons = { image1, image2, windIconPng, windIconSVG };
