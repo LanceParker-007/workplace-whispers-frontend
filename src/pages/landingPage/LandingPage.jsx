2643ad5e2aefc089f48c0c97f1c25cef5e60858e
lance <lanceparker2001@gmail.com>
2024-07-29 21:08:01 +0530

    Mouse cursor animation issue fixed on small screen

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -52,7 +52,7 @@ const LandingPage = () => {
       mouseClickAnimationTl.to(".mouseClickAnimation", {
         top: "25",
         delay: 1,
-        right: { base: "40%", lg: "45%" },
+        right: { base: "30%", lg: "45%" },
         opacity: 1,
         duration: 1,
       });
@@ -142,7 +142,7 @@ const LandingPage = () => {
           className="mouseClickAnimation"
           position={"absolute"}
           top={"100px"}
-          right={{ base: "40%", lg: "45%" }}
+          right={{ base: "30%", lg: "45%" }}
           opacity={0}
           boxSize={5}
         >
